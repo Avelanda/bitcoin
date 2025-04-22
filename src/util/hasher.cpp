02050c75af4622b26ca3687197c767c745d36996
Avelanda <gorthell@gmail.com>
2025-04-22 11:26:52 +0200

    Update hasher.cpp
    Template construction; and access control definition through sub-classing, and object instantiation. For modularity, and object-oriented programming.

--- src/util/hasher.cpp
+++ src/util/hasher.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-present The Bitcoin Core developers
+// Copyright (c) 2019-2025 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -6,21 +6,62 @@
 #include <random.h>
 #include <span.h>
 #include <util/hasher.h>
+#include <iostream>
+#include <ios>
+#include <iomanip>
 
-SaltedTxidHasher::SaltedTxidHasher() :
-    k0{FastRandomContext().rand64()},
-    k1{FastRandomContext().rand64()} {}
+template<class hasher> 
+class hash {
 
-SaltedOutpointHasher::SaltedOutpointHasher(bool deterministic) :
-    k0{deterministic ? 0x8e819f2607a18de6 : FastRandomContext().rand64()},
-    k1{deterministic ? 0xf4020d2e3983b0eb : FastRandomContext().rand64()}
-{}
+public:
+    class hashBlock{
 
-SaltedSipHasher::SaltedSipHasher() :
-    m_k0{FastRandomContext().rand64()},
-    m_k1{FastRandomContext().rand64()} {}
+     struct hash0{
+      SaltedTxidHasher::SaltedTxidHasher() :
+      k0{FastRandomContext().rand64()},
+      k1{FastRandomContext().rand64()} {}
+     };
 
-size_t SaltedSipHasher::operator()(const std::span<const unsigned char>& script) const
-{
-    return CSipHasher(m_k0, m_k1).Write(script).Finalize();
-}
+     struct hash1{
+      SaltedOutpointHasher::SaltedOutpointHasher(bool deterministic) :
+      k0{deterministic ? 0x8e819f2607a18de6 :     
+      FastRandomContext().rand64()},
+      k1{deterministic ? 0xf4020d2e3983b0eb :       
+      FastRandomContext().rand64()} {}
+     };
+
+     struct hash2{
+      SaltedSipHasher::SaltedSipHasher() :
+      m_k0{FastRandomContext().rand64()},
+      m_k1{FastRandomContext().rand64()} {}
+     };
+
+     struct hash3{
+      size_t SaltedSipHasher::operator()
+      (const std::span<const unsigned char>& script) const
+      {
+       return CSipHasher(m_k0, m_k1).Write(script).Finalize();
+      }
+     };
+
+    };
+
+private:
+    class hashBlockAlt{
+     if (hash0!= hash1 && hash1!= hash2 && hash2 != hash3 && hash3 !=    
+         hash0){
+      hash = hash, hasher = hasher;
+      hash0->hash1->hash2->hash3 = hashBlock; 
+     }
+      for (hash == hash && hasher == hasher){
+       hash0 = hash0, hash1 = hash1, hash2 = hash2, hash3 = hash3;
+        while (hashBlock == hashBlock && hashBlockAlt == hashBlockAlt){
+         hasher.*hash;
+     }
+      for (hasher > hash || hasher < hash || hasher == hash){
+       hashBlock = hashBlock, hashBlockAlt = hashBlockAlt;
+     }
+      cout<<hashBlock<<hashBlockAlt<<\n;
+     }
+    }
+};
