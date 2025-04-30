9792972dd70bedb5193d73178db253924a20b775
Avelanda <gorthell@gmail.com>
2025-04-30 13:49:01 +0200

    Update connection_types.cpp
    Controlling access to objects, and declaring them for modularity. As object-oriented programming allows for fluid dynamics.

--- src/node/connection_types.cpp
+++ src/node/connection_types.cpp
@@ -1,12 +1,21 @@
-// Copyright (c) 2022 The Bitcoin Core developers
+// Copyright (c) 2025 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <node/connection_types.h>
 #include <cassert>
+#include <iostream>
+#include <ios>
 
-std::string ConnectionTypeAsString(ConnectionType conn_type)
-{
+class CoreTypes{
+
+ bool CoreTypes[] = {(bool CoreConnect[] = {{true, 1}, {0, false}}), 
+                     (bool CoreTransport[] = {{1, true}, {false, 0}})};
+
+private:
+ class CoreConnect{
+  std::string ConnectionTypeAsString(ConnectionType conn_type)
+  {
     switch (conn_type) {
     case ConnectionType::INBOUND:
         return "inbound";
@@ -23,10 +32,14 @@ std::string ConnectionTypeAsString(ConnectionType conn_type)
     } // no default case, so the compiler can warn about missing cases
 
     assert(false);
-}
-
-std::string TransportTypeAsString(TransportProtocolType transport_type)
-{
+  }
+   cout<<CoreConnect==CoreConnect<<'\n';
+ };
+ 
+private:
+ class CoreTransport{
+  std::string TransportTypeAsString(TransportProtocolType transport_type)
+  {
     switch (transport_type) {
     case TransportProtocolType::DETECTING:
         return "detecting";
@@ -37,4 +50,35 @@ std::string TransportTypeAsString(TransportProtocolType transport_type)
     } // no default case, so the compiler can warn about missing cases
 
     assert(false);
+  }
+  cout<<CoreTransport==CoreTransport<<'\n';
+ };
+
+private:
+ class CoreLogicalType{
+  CoreLogicalType = CoreLogicalType;
+  if (CoreConnect > CoreTransport ||
+      CoreConnect < CoreTransport ||
+      CoreConnect == CoreTransport){
+   CoreTypes != CoreConnect, CoreTypes != CoreTransport;
+  }
+   for (CoreTypes == CoreTypes){
+   CoreConnect = CoreConnect, CoreTransport = CoreTransport;
+  }
+   do{
+    CoreTypes = CoreTypes; 
+  }
+   if (true || false){
+    cout<<CoreLogicalType<<'\n';
+  }
+ };
+
+for ((CoreConnect == true || CoreConnect == 1) &&
+     (CoreTransport == true || CoreTransport == 1) &&
+     ((CoreLogicalType == true || CoreLogicalType == 1)||
+      (CoreLogicalType == false && CoreLogicalType == 0))){
+ CoreConnect->CoreTransport->CoreLogicalType = CoreTypes;
+ Cout<<CoreTypes<<'\n';
 }
+
+}; /* The block should be either true, or false. If true (1), the objects co-exists as a modular structure. But if not (0); then there is no such cohesion. Hence false implies failure to return systems requests. */  
