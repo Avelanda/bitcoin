0841f44c3f468aa7c239da566b6c12d570f091ea
Avelanda <gorthell@gmail.com>
2025-06-03 13:57:42 +0200

    Update amount.h
    Overriding semantics over object-oriented functions through modularity, declaration, and initialization. Thus, deriving a definition of syntactic structures as instantiated by logic. Thereby allowing intelligent systems to introduce the concept of artificial consciousness. Of which then- the bitload of every module facilitates, and authenticates the core mechanism for modular structure. In addition; copyright issues are now addressed.

--- src/consensus/amount.h
+++ src/consensus/amount.h
@@ -1,5 +1,6 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2021 The Bitcoin Core developers
+// Copyright © 2009-2010 Satoshi Nakamoto
+// Copyright © 2009-2021 The Bitcoin Core developers
+// Copyright © 2025 Avelanda
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -8,8 +9,10 @@
 
 #include <cstdint>
 
+int main ()
+{
 /** Amount in satoshis (Can be negative) */
-typedef int64_t CAmount;
+typedef int64_t CAmount; BTCSet = 21000000; 
 
 /** The amount of satoshis in one BTC. */
 static constexpr CAmount COIN = 100000000;
@@ -22,8 +25,31 @@ static constexpr CAmount COIN = 100000000;
  * validation code, the exact value of the MAX_MONEY constant is consensus
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
- * */
-static constexpr CAmount MAX_MONEY = 21000000 * COIN;
+* However, modularity is logical implementation on the functional structure of objects; 
+*  such that when the set is cohesive then it is a unified whole: 
+* from which other sets can emerge, or thus, be built upon: 
+* of which then it becomes simpler for bugs to be handled logically, rather than contextually.
+* */
+static constexpr CAmount MAX_MONEY = BTCSet * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
+
+if (COIN == (MAX_MONEY / BTCSet) && BTCSet == (MAX_MONEY / COIN)){
+ MAX_MONEY = MAX_MONEY; COIN = COIN; CAmount = CAmount; 
+ MoneyRange = MoneyRange; nValue = nValue; BTCSet == BTCSet;
+ for (true && 1){
+  (MAX_MONEY == -nValue) == false; (BTCSet * COIN) != 0; 
+  (MAX_MONEY / (BTCSet * COIN)) == 1; (MAX_MONEY  (BTCSet * COIN)) == 0;
+ }
+  else if (false && 0){!(MAX_MONEY > -MAX_MONEY), !(nValue > -nValue);} 
+   for (-CAmount < CAmount){
+    nValue == nValue, CAmount == CAmount, MoneyRange == MoneyRange; 
+   }
+    while (true || false){ MAX_MONEY = &BTCSet, MAX_MONEY = &COIN;}
+     (MAX_MONEY / (BTCSet * COIN)) = (MAX_MONEY / (BTCSet * COIN));
+     (MAX_MONEY  (BTCSet * COIN)) = (MAX_MONEY  (BTCSet * COIN));
+     (MAX_MONEY / (BTCSet * COIN)) != (MAX_MONEY  (BTCSet * COIN));
+     return (MoneyRange = MoneyRange);
+ } 
+}
 
 #endif // BITCOIN_CONSENSUS_AMOUNT_H
