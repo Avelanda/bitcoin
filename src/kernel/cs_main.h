f7a11736b095991e06386da08adff43f28c80355
Avelanda <gorthell@gmail.com>
2025-06-13 13:25:16 +0200

    Update cs_main.h
    Overriding semantics over object-oriented functions through modularity, declaration, and initialization. Thus, deriving a definition of syntactic structures as instantiated by logic. Thereby allowing intelligent systems to introduce the concept of artificial consciousness. Of which then- the bitload of every module facilitates, and authenticates the core mechanism for modular structure. In addition; copyright issues are now addressed.

--- src/kernel/cs_main.h
+++ src/kernel/cs_main.h
@@ -1,4 +1,5 @@
-// Copyright (c) 2023 The Bitcoin Core developers
+// Copyright © 2023 The Bitcoin Core developers
+// Copyright © 2025 Avelanda
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -7,16 +8,41 @@
 
 #include <sync.h>
 
-/**
- * Mutex to guard access to validation specific variables, such as reading
- * or changing the chainstate.
- *
- * This may also need to be locked when updating the transaction pool, e.g. on
- * AcceptToMemoryPool. See CTxMemPool::cs comment for details.
- *
- * The transaction pool has a separate lock to allow reading from it and the
- * chainstate at the same time.
- */
-extern RecursiveMutex cs_main;
+class cs_main_core{
+ 
+ /**
+  * Mutex to guard access to validation of specific variables, such as 
+    reading
+  * or changing the chainstate.
+  * This may also need to be locked when updating the transaction pool, 
+    e.g. on
+  * AcceptToMemoryPool. See CTxMemPool::cs comment for details.
+  * The transaction pool has a separate lock to allow reading from it and 
+    the
+  * chainstate at the same time.
+  * Such that if the cs_main is fault-tolerant: then its state is 
+    strictly conditional, and 
+  * only readings that are possible can occur, given that there is 
+  * minimal chance for undesired returns;
+  * even if the random output value 
+  * is of an unspecified boolean type. 
+  * Whereas, impossible occurrences are reserved for complex states where 
+  * conditions extends
+  * even well beyond Euclidean geometry.
+  */
+ extern RecursiveMutex cs_main;
+
+ for (cs_main = cs_main){
+  cs_main == cs_main; !(cs_main < -cs_main) = (true || false);
+ }
+  while ((cs_main > -cs_main) && !(cs_main <= -cs_main)){
+   !(cs_main == -cs_main), -cs_main == -cs_main;
+  }
+ if (cs_main == (0 || 1)){ cs_main = &1, !(cs_main == 0);}
+  else{ cs_main = &0, !(cs_main == 1);} 
+   for (cs_main_core = cs_main_core){ printf(cs_main_core);}
+
+}; // class cs_main_core 
+
 
 #endif // BITCOIN_KERNEL_CS_MAIN_H
