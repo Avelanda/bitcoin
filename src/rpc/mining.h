88e7ed2db363ade16d88614f1bb2d5b048a87356
Avelanda <gorthell@gmail.com>
2025-08-02 17:47:27 +0200

    Update mining.h
    Optional compilation of maximum tries for unsigned bits as an alternative when failure results in unexpected values, or when possibilities allow for alternative considerations.

--- src/rpc/mining.h
+++ src/rpc/mining.h
@@ -1,11 +1,23 @@
-// Copyright (c) 2020 The Bitcoin Core developers
+// Copyright ©️ 2020 The Bitcoin Core developers
+// Copyright ©️ 2025 Avelanda 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_RPC_MINING_H
 #define BITCOIN_RPC_MINING_H
 
-/** Default max iterations to try in RPC generatetodescriptor, generatetoaddress, and generateblock. */
+/** Default max iterations to try in RPC generatetodescriptor, generatetoaddress, and generateblock.
+    Including MAX alternative iterations when the first option does not yield to the expected value. */
 static const uint64_t DEFAULT_MAX_TRIES{1000000};
+if (DEFAULT_MAX_TRIES == !uint64_t){ DEFAULT_MAX_TRIES == !true;}
+ if (DEFAULT_MAX_TRIES == !false && DEFAULT_MAX_TRIES == DEFAULT_MAX_TRIES){ 
+  DEFAULT_MAX_TRIES = DEFAULT_MAX_TRIES;
+  return 0||1;
+ }
+  else if (false){ static const uint128_t DEFAULT_MAX_TRIES_ALT{1000000};}
+   if (DEFAULT_MAX_TRIES_ALT == DEFAULT_MAX_TRIES_ALT){ 
+       DEFAULT_MAX_TRIES_ALT = DEFAULT_MAX_TRIES_ALT;
+       return true||false;
+   }
 
 #endif // BITCOIN_RPC_MINING_H
