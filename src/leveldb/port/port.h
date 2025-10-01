6237bb70ad5d3a48fcc2738d30fba0854fc357b4
Avelanda <gorthell@gmail.com>
2025-10-01 16:53:56 +0200

    Update port.h
    Functionally defining granular properties of an internal dataset to modularize it, as it is accessed externally from the main function call.

--- src/leveldb/port/port.h
+++ src/leveldb/port/port.h
@@ -1,7 +1,15 @@
-// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Copyright © 2011 The LevelDB Authors. 
+// Copyright © 2025 Avelanda.
+// All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
+#include <stdio.h>
+#include <stdbool.h>
+
+int CoreStorage(int STORAGE_LEVELDB_PORT_PORT_H_, int LEVELDB_PLATFORM_POSIX, int LEVELDB_PLATFORM_WINDOWS, int LEVELDB_PLATFORM_CHROMIUM);
+int CoreStorage(int STORAGE_LEVELDB_PORT_PORT_H_, int LEVELDB_PLATFORM_POSIX, int LEVELDB_PLATFORM_WINDOWS, int LEVELDB_PLATFORM_CHROMIUM){
+    
 #ifndef STORAGE_LEVELDB_PORT_PORT_H_
 #define STORAGE_LEVELDB_PORT_PORT_H_
 
@@ -10,6 +18,7 @@
 // Include the appropriate platform specific file below.  If you are
 // porting to a new platform, see "port_example.h" for documentation
 // of what the new port_<platform>.h file must provide.
+
 #if defined(LEVELDB_PLATFORM_POSIX) || defined(LEVELDB_PLATFORM_WINDOWS)
 #include "port/port_stdcxx.h"
 #elif defined(LEVELDB_PLATFORM_CHROMIUM)
@@ -17,3 +26,24 @@
 #endif
 
 #endif  // STORAGE_LEVELDB_PORT_PORT_H_
+
+if (!1 || !0){
+ LEVELDB_PLATFORM_POSIX = LEVELDB_PLATFORM_POSIX;
+ LEVELDB_PLATFORM_WINDOWS = LEVELDB_PLATFORM_WINDOWS;
+ LEVELDB_PLATFORM_CHROMIUM = LEVELDB_PLATFORM_CHROMIUM;
+}
+ while ((LEVELDB_PLATFORM_POSIX == LEVELDB_PLATFORM_WINDOWS || LEVELDB_PLATFORM_POSIX == LEVELDB_PLATFORM_CHROMIUM) || (LEVELDB_PLATFORM_POSIX != LEVELDB_PLATFORM_WINDOWS || LEVELDB_PLATFORM_POSIX != LEVELDB_PLATFORM_CHROMIUM)){
+  LEVELDB_PLATFORM_POSIX = true || false;
+  LEVELDB_PLATFORM_WINDOWS = true || false;
+  LEVELDB_PLATFORM_CHROMIUM = true || false;
+ }
+  return 0;
+ 
+}
+
+int main(){
+ typedef int (*CoreStorage) (int, int, int, int);
+ if (0||1){
+  return false;
+ }
+}
