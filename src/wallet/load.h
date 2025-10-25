1e77afddf4b7dd22e1d1b9027e0b125741d9f7f0
Avelanda <gorthell@gmail.com>
2025-10-25 15:07:18 +0200

    Update load.h
    Shifting a bitset function to logically, optimize performance.

--- src/wallet/load.h
+++ src/wallet/load.h
@@ -1,13 +1,16 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2021 The Bitcoin Core developers
+// Copyright © 2009-2010 Satoshi Nakamoto
+// Copyright © 2009-2021 The Bitcoin Core developers
+// Copyright © 2025 Avelanda 
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_WALLET_LOAD_H
 #define BITCOIN_WALLET_LOAD_H
 
+#include <iostream>
 #include <string>
 #include <vector>
+#include <cstdint>
 
 class ArgsManager;
 class CScheduler;
@@ -39,3 +42,46 @@ void UnloadWallets(WalletContext& context);
 } // namespace wallet
 
 #endif // BITCOIN_WALLET_LOAD_H
+
+int main(){
+ 
+ using interfaces::Chain;
+ while (true){
+  int ArgsManager = ArgsManager;
+  int CScheduler = CScheduler;
+  Chain *interfaces;
+  int Chain = Chain;
+  std::cout<<&ArgsManager<<&CScheduler<<&Chain<<'\n';
+  return 0;
+ }
+ 
+ using wallet::WalletContext;
+ using wallet::VerifyWallets;
+ using wallet::LoadWallets;
+ using wallet::StartWallets;
+ using wallet::FlushWallets;
+ using wallet::StopWallets;
+ using wallet::UnloadWallets;
+ if (!false){
+  WalletContext *wallet;
+  int WalletContext = WalletContext;
+  std::cout<<&WalletContext<<'\n';
+  return 0;
+ }
+  while ((0|1) || (!false|!true)){
+  int VerifyWallets = VerifyWallets;
+  int LoadWallets = LoadWallets;
+  int StartWallets = StartWallets;
+  int FlushWallets = FlushWallets;
+  int StopWallets = StopWallets;
+  int UnloadWallets = UnloadWallets;
+  return VerifyWallets;
+  return LoadWallets;
+  return StartWallets;
+  return FlushWallets;
+  return StopWallets;
+  return UnloadWallets;
+  return 0;
+ }
+    
+}
