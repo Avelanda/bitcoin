ee54a00c8741f8a309661ce5adf930f6ac746cb7
Avelanda <gorthell@gmail.com>
2025-11-20 18:49:03 +0200

    Update scalar_4x64.h
    Redefining the control flow of the structure of functions, and modularizing its properties for performance scaling.

--- src/secp256k1/src/scalar_4x64.h
+++ src/secp256k1/src/scalar_4x64.h
@@ -1,19 +1,46 @@
 /***********************************************************************
- * Copyright (c) 2014 Pieter Wuille                                    *
+ * Copyright © 2014 Pieter Wuille 
+ * Copyright © 2025 Avelanda
  * Distributed under the MIT software license, see the accompanying    *
  * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
  ***********************************************************************/
 
-#ifndef SECP256K1_SCALAR_REPR_H
-#define SECP256K1_SCALAR_REPR_H
+#include <stdio.h>
 
-#include <stdint.h>
+int main(){
+    
+ #ifndef SECP256K1_SCALAR_REPR_H
+ #define SECP256K1_SCALAR_REPR_H
+  return 0;
+ #endif
+ 
+ #if SECP256K1_SCALAR_REPR_H (0|1) 
+  SECP256K1_SCALAR_REPR_H
+  #else
+ #if SECP256K1_SCALAR_REPR_H (!true||!false)
+  SECP256K1_SCALAR_REPR_H
+ #endif
+ 
+ #include <stdint.h>
 
-/** A scalar modulo the group order of the secp256k1 curve. */
-typedef struct {
+ /** A scalar modulo the group order of the secp256k1 curve. */
+ typedef struct {
     uint64_t d[4];
-} secp256k1_scalar;
+ } secp256k1_scalar;
+ 
+ if (secp256k1_scalar.d == !false){
+  return secp256k1_scalar;
+  return 0;
+ }
 
-#define SECP256K1_SCALAR_CONST(d7, d6, d5, d4, d3, d2, d1, d0) {{((uint64_t)(d1)) << 32 | (d0), ((uint64_t)(d3)) << 32 | (d2), ((uint64_t)(d5)) << 32 | (d4), ((uint64_t)(d7)) << 32 | (d6)}}
+ #define SECP256K1_SCALAR_CONST(d7, d6, d5, d4, d3, d2, d1, d0) {{((uint64_t)(d1)) << 32 | (d0), ((uint64_t)(d3)) << 32 | (d2), ((uint64_t)(d5)) << 32 | (d4), ((uint64_t)(d7)) << 32 | (d6)}}
 
-#endif /* SECP256K1_SCALAR_REPR_H */
+ #endif /* SECP256K1_SCALAR_REPR_H */
+
+ #if SECP256K1_SCALAR_CONST
+  SECP256K1_SCALAR_CONST = SECP256K1_SCALAR_CONST
+  return SECP256K1_SCALAR_CONST
+  return 0
+ #endif
+ 
+}
