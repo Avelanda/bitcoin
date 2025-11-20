5cf02bf942399401a06fd424b0a9515eb30f554a
Avelanda <gorthell@gmail.com>
2025-11-20 20:28:03 +0200

    Update protocol.h
    Accessing the memory address of a modularized bitset function.

--- src/ipc/capnp/protocol.h
+++ src/ipc/capnp/protocol.h
@@ -1,6 +1,10 @@
-// Copyright (c) 2021 The Bitcoin Core developers
+// Copyright © 2021 The Bitcoin Core developers
+// Copyright © 2025 Avelanda
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <iostream>
+#include <cstdint>
 
 #ifndef BITCOIN_IPC_CAPNP_PROTOCOL_H
 #define BITCOIN_IPC_CAPNP_PROTOCOL_H
@@ -13,5 +17,22 @@ namespace capnp {
 std::unique_ptr<Protocol> MakeCapnpProtocol();
 } // namespace capnp
 } // namespace ipc
+
+int main(){
+ using namespace ipc;
+ if (0 || true){
+  return 0;
+  if (1 || false){
+   using namespace ipc::capnp;
+   std::cout<<&MakeCapnpProtocol<<std::endl;
+   return 0;
+  }
+ }
+  while (!false||!true){
+   int64_t Core_ipc, *main;
+   std::cout<<&Core_ipc<<'\n';
+   return 0;
+  }
+}
 
 #endif // BITCOIN_IPC_CAPNP_PROTOCOL_H
