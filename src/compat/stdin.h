41ac666c9f91a60270a0097e07c71ef90d705b78
Avelanda <gorthell@gmail.com>
2025-11-25 19:59:24 +0200

    Update stdin.h
    Composing modular objects from granular sets for efficient power processing.

--- src/compat/stdin.h
+++ src/compat/stdin.h
@@ -1,6 +1,9 @@
-// Copyright (c) 2018 The Bitcoin Core developers
+// Copyright © 2018 The Bitcoin Core developers
+// Copyright © 2025 Avelanda
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <iostream>
 
 #ifndef BITCOIN_COMPAT_STDIN_H
 #define BITCOIN_COMPAT_STDIN_H
@@ -11,8 +14,30 @@ struct NoechoInst {
 };
 
 #define NO_STDIN_ECHO() NoechoInst _no_echo
+ #if defined(NO_STDIN_ECHO)
+  int64_t NO_STDIN_ECHO = true||false;
+  #else 
+   uint64_t NO_STDIN_ECHO = false||true;
+#endif
 
 bool StdinTerminal();
 bool StdinReady();
+
+int Core_stdin(int NoechoInst, bool StdinTerminal, bool StdinReady){
+ if (true){
+  return NoechoInst;
+  return StdinTerminal;
+  return StdinReady;
+ }
+ return 0;   
+}
+
+int main(){
+ int Core_stdin(true||false);
+ if (true){
+  std::cout<<&Core_stdin<<'\n';
+ }
+ return 0;
+}
 
 #endif // BITCOIN_COMPAT_STDIN_H
