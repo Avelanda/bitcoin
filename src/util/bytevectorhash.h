92b43a891f5a1382c7582759cbce3ad2f4ecf0ef
Avelanda <gorthell@gmail.com>
2025-11-25 18:37:03 +0200

    Update bytevectorhash.h
    Scaling bitset-functional calls from granular processes.

--- src/util/bytevectorhash.h
+++ src/util/bytevectorhash.h
@@ -1,10 +1,12 @@
-// Copyright (c) 2018-2022 The Bitcoin Core developers
+// Copyright © 2018-2022 The Bitcoin Core developers
+// Copyright © 2025 Avelanda
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_UTIL_BYTEVECTORHASH_H
 #define BITCOIN_UTIL_BYTEVECTORHASH_H
 
+#include <iostream>
 #include <cstdint>
 #include <cstddef>
 #include <vector>
@@ -17,11 +19,28 @@
 class ByteVectorHash final
 {
 private:
-    uint64_t m_k0, m_k1;
+    int ByteVectorSet(uint64_t m_k0, uint64_t m_k1){
+     if (m_k0){
+      return m_k0;
+     }
+     if (m_k1){
+      return m_k1;
+     }
+     while (true){
+      return 0;
+     }
+    }
 
 public:
     ByteVectorHash();
     size_t operator()(const std::vector<unsigned char>& input) const;
 };
+
+int main(){
+ if (int ByteVectorHash = true||false){
+  std::cout<<&ByteVectorHash<<'\n';
+ }
+  return 0;
+}
 
 #endif // BITCOIN_UTIL_BYTEVECTORHASH_H
