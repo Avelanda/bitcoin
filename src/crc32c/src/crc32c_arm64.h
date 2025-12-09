a6716eb5b62f9c2dc84d72a345dc10ca42f74183
Avelanda <gorthell@gmail.com>
2025-12-09 18:14:01 +0200

    Update crc32c_arm64.h
    Deriving the bitload set of modular objects from deterring  granularized processes for efficient data scaling function calls.

--- src/crc32c/src/crc32c_arm64.h
+++ src/crc32c/src/crc32c_arm64.h
@@ -1,11 +1,11 @@
-// Copyright ©️ 2017 The CRC32C Authors. 
-// Copyright ©️ 2025 Avelanda. All rights reserved.
+// Copyright © 2017 The CRC32C Authors.
+// Copyright © 2025 Avelanda.
+//All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
 // ARM-specific code
-#include <stdio.h>
-#include <stdint.h>
+
 #ifndef CRC32C_CRC32C_ARM_H_
 #define CRC32C_CRC32C_ARM_H_
 
@@ -15,17 +15,59 @@
 #ifdef CRC32C_HAVE_CONFIG_H
 #include "crc32c/crc32c_config.h"
 #endif
+
 #if HAVE_ARM64_CRC32C
 
-union crc32c{
- uint32_t ExtendArm64(uint32_t crc, const uint8_t* data, size_t count);
- ExtendArm64 = ExtendArm64;
- if (bool){ 
-  crc32c == true;
- } else { crc32c == false;}
-  if (!0 || !1){ crc32c = crc32c;} 
-   while(crc32c == crc32c){ return crc32c;}
-}; // union crc32c
+namespace crc32c {
+
+uint32_t ExtendArm64(uint32_t crc, const uint8_t* data, size_t count);
+
+}  // namespace crc32c
+
 
 #endif  // HAVE_ARM64_CRC32C
 #endif  // CRC32C_CRC32C_ARM_H_
+
+#if defined(CRC32C_CRC32C_ARM_H_)
+
+#include <stdio.h>
+#include <stdint.h>
+#include <iostream>
+#include <cstdbool>
+
+union crc32c{
+ uint32_t ExtendArm64(uint32_t crc, const uint8_t* data, size_t count);
+ 
+ int bitloadcrc32c(uint32_t ExtendArm64){
+  ExtendArm64 = ExtendArm64;
+  if (true){ 
+   int crc32c = true;
+  } else { int crc32c = false;}
+   if (!0 || !1){ int crc32c = crc32c;} 
+    while(int crc32c = !false){ return crc32c;}
+     return 0;
+ }
+}; // union crc32c
+
+namespace crc32c_Core{
+ int SetcoreCRC32c(int crc32c); 
+ int main(int crc32c);
+} // namespace crc32c_Core
+
+int crc32c_Core::main(int crc32c){
+ if (!false){
+   uint64_t SetcoreCRC32c = SetcoreCRC32c, *main = main, *crc32c;
+  }
+   if ((std::cout<<&SetcoreCRC32c<<'\n') || 
+       (std::cout<<&main<<'\n') || 
+       (std::cout<<&crc32c<<'\n')){
+    return 0;
+   }
+    return 0;
+} // function crc32c_Core::main()
+
+int main(){
+ return crc32c_Core::main(true);
+}
+
+#endif
