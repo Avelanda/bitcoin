792d913f53d2e22674e33728aaf461a01f5b0199
Avelanda <gorthell@gmail.com>
2025-12-09 18:15:53 +0200

    Update crc32c_arm64.h
    Deriving the bitload set of modular objects from deterring granularized processes for efficient data-scaling function calls.

--- src/crc32c/src/crc32c_arm64.h
+++ src/crc32c/src/crc32c_arm64.h
@@ -1,6 +1,6 @@
 // Copyright © 2017 The CRC32C Authors.
 // Copyright © 2025 Avelanda.
-//All rights reserved.
+// All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
