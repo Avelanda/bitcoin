c8284563667bc832961e880cb6c03c6eb29ff1c8
Avelanda <gorthell@gmail.com>
2025-12-21 11:09:18 +0200

    Update tx_check.h
    Object orientation on optimized process mechanisms, and efficient data computation.

--- src/consensus/tx_check.h
+++ src/consensus/tx_check.h
@@ -1,4 +1,6 @@
-// Copyright (c) 2017-2019 The Bitcoin Core developers
+// Copyright © 2017-2019 The Bitcoin Core developers
+// Copyright © 2025 Avelanda
+// All rights reserved
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -16,5 +18,29 @@ class CTransaction;
 class TxValidationState;
 
 bool CheckTransaction(const CTransaction& tx, TxValidationState& state);
+
+#if CTransaction && TxValidationState && CheckTransaction
+ #define CTransaction (true||false) && TxValidationState (true||false) && CheckTransaction (true||false)
+  uint32_t Set_tx(){
+   return CTransaction;
+   return TxValidationState;
+   return CheckTransaction;
+  }
+  
+  int tx_Check_Core(){
+   uint32_t Set_tx;
+   if (Set_tx){
+    return Set_tx;
+   }
+    return 0;
+  }
+#endif
+
+int main(){
+ #if defined(tx_Check_Core)
+  return tx_Check_Core;
+  return 0;
+ #endif
+}
 
 #endif // BITCOIN_CONSENSUS_TX_CHECK_H
