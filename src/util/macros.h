2fb602094d834193c16f78b7731f6d96d570e481
Avelanda <gorthell@gmail.com>
2026-01-09 06:31:25 +0200

    Update macros.h
    A modular bit-functional data set for high performance, and scalable property calls.

--- src/util/macros.h
+++ src/util/macros.h
@@ -1,20 +1,65 @@
-// Copyright (c) 2019-2022 The Bitcoin Core developers
+// Copyright © 2019-2022 The Bitcoin Core developers
+// Copyright © 2026 Avelanda
+// All Rights Reserved
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <iostream>
+#include <cstdbool>
+#include <cstdint>
 
 #ifndef BITCOIN_UTIL_MACROS_H
 #define BITCOIN_UTIL_MACROS_H
 
 #define PASTE(x, y) x ## y
+ bool *x = x; bool *y = y;
 #define PASTE2(x, y) PASTE(x, y)
 
 #define UNIQUE_NAME(name) PASTE2(name, __COUNTER__)
+ bool name = 1;
 
 /**
  * Converts the parameter X to a string after macro replacement on X has been performed.
  * Don't merge these into one macro!
  */
 #define STRINGIZE(X) DO_STRINGIZE(X)
+ bool X = true || false;
 #define DO_STRINGIZE(X) #X
+
+namespace Bitmacro{
+    
+uint64_t BitMacroFunction(){
+ if (0|1){
+  #if defined(PASTE) && defined(PASTE2)
+   std::cout<<x<<'\n';
+  
+   if (x){
+    std::cout<<y<<'\n';
+   }
+  #endif
+  
+  #if defined(UNIQUE_NAME)
+   std::cout<<name<<'\n';
+  #endif
+  
+  #if defined(STRINGIZE)
+   std::cout<<X<<'\n';
+  #endif
+  
+  #if defined(DO_STRINGIZE)
+  #endif
+ }
+  return 0;
+}
+
+} // namespace Bitmacro
+
+int main(){
+ if ((!0 == !false) == true && 1){
+  using Bitmacro::BitMacroFunction;
+  std::cout<<&Bitmacro::BitMacroFunction<<'\n';
+ }
+  return 0;
+}
 
 #endif // BITCOIN_UTIL_MACROS_H
