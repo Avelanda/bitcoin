b2329364a038a70284aac6ad86eb967454bf8dfe
Avelanda <gorthell@gmail.com>
2026-03-07 17:15:05 +0200

    Update cleanse.h
    Modular oriented programming on highly optimized bitset-function calls.

--- src/support/cleanse.h
+++ src/support/cleanse.h
@@ -1,15 +1,54 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Copyright  ©  2009-2010 Satoshi Nakamoto
+// Copyright  ©  2009-2022 The Bitcoin Core developers
+// Copyright  ©  2026 Avelanda
+// All rights reserved
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_SUPPORT_CLEANSE_H
 #define BITCOIN_SUPPORT_CLEANSE_H
 
+#include <iostream>
 #include <cstdlib>
+#include <cstdint>
 
 /** Secure overwrite a buffer (possibly containing secret data) with zero-bytes. The write
  * operation will not be optimized out by the compiler. */
-void memory_cleanse(void *ptr, size_t len);
+
+class MemoryCore{
+ public:
+  void memory_cleanse(void *ptr, size_t len);
+  
+  volatile static uint64_t MCset(){
+   do {
+    uint32_t *memory_cleanse(void);
+    uint32_t main = main;
+   
+    if ((false && 0) | (true && 1)){
+     std::cout<<memory_cleanse();
+    }
+   }
+    while (0|1);
+     return 0;
+  }
+ 
+  uint64_t MCSetFile(){
+   if (MCset()){
+    return MCset();
+   }
+    return 0;
+  }
+};
+
+#if MemoryCore
+ #define MemoryCore (true && 1)
+  return MemoryCore;
+#endif
+
+int main(){
+ if (&MemoryCore::MCSetFile){
+  return 0;
+ }
+}
 
 #endif // BITCOIN_SUPPORT_CLEANSE_H
