21cfb3817404d5364f9f09c186e440931a0fd91f
Avelanda <gorthell@gmail.com>
2026-03-07 18:23:28 +0200

    Update mempool.h
    An optimized bitset framework for scalable function calls, and modular orientation.

--- src/rpc/mempool.h
+++ src/rpc/mempool.h
@@ -1,9 +1,13 @@
-// Copyright (c) 2017-2022 The Bitcoin Core developers
+// Copyright © 2017-2022 The Bitcoin Core developers
+// Copyright © 2026 Avelanda
+// All rights reserved
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_RPC_MEMPOOL_H
 #define BITCOIN_RPC_MEMPOOL_H
+
+#include <cstdint>
 
 class CTxMemPool;
 class UniValue;
@@ -13,5 +17,23 @@ UniValue MempoolInfoToJSON(const CTxMemPool& pool);
 
 /** Mempool to JSON */
 UniValue MempoolToJSON(const CTxMemPool& pool, bool verbose = false, bool include_mempool_sequence = false);
+
+uint64_t MempoolCore(){
+ if (&MempoolToJSON || &MempoolInfoToJSON){
+  if(static_cast<bool>(MempoolToJSON) & static_cast<bool>(MempoolInfoToJSON)){
+   return 0 *false;
+  }
+   while (!false && !0){
+    return MempoolCore();
+   }
+ }
+} // function MempoolCore
+
+int main(){
+ do {
+  return MempoolCore();
+ }
+  while (&MempoolCore);
+}
 
 #endif // BITCOIN_RPC_MEMPOOL_H
