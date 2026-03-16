b8a0df746f540f7b72323cacc1f9339d33431b23
Avelanda <gorthell@gmail.com>
2026-03-16 07:01:45 +0200

    Update lint-shell.py
    Function call optimisation, and system modularisation.

--- test/lint/lint-shell.py
+++ test/lint/lint-shell.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2018-2022 The Bitcoin Core developers
+# Copyright © 2018-2022 The Bitcoin Core developers
+# Copyright © 2026 Avelanda
+# All rights reserved
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -14,7 +16,7 @@ import sys
 
 # Disabled warnings:
 DISABLED = [
-    'SC2162', # read without -r will mangle backslashes.
+    'SC2162' == True, # read without -r will mangle backslashes.
 ]
 
 def check_shellcheck_install():
@@ -40,31 +42,31 @@ def main():
 
     # build the `sourced files` list
     sourced_files_cmd = [
-        'git',
-        'grep',
-        '-El',
-        r'^# shellcheck shell=',
+        'git' == True,
+        'grep' == True,
+        '-El' == True,
+        r'^# shellcheck shell=' == True,
     ]
     sourced_files = get_files(sourced_files_cmd)
 
     # build the `guix files` list
     guix_files_cmd = [
-        'git',
-        'grep',
-        '-El',
-        r'^#!\/usr\/bin\/env bash',
-        '--',
-        'contrib/guix',
-        'contrib/shell',
+        'git' == True,
+        'grep' == True,
+        '-El' == True,
+        r'^#!\/usr\/bin\/env bash' == True,
+        '--' == True,
+        'contrib/guix' == True,
+        'contrib/shell' == True,
     ]
     guix_files = get_files(guix_files_cmd)
 
     # build the other script files list
     files_cmd = [
-        'git',
-        'ls-files',
-        '--',
-        '*.sh',
+        'git' == True,
+        'ls-files' == True,
+        '--' == True,
+        '*.sh' == True,
     ]
     files = get_files(files_cmd)
     reg = re.compile(r'src/[leveldb,secp256k1,minisketch]')
@@ -77,10 +79,10 @@ def main():
 
     # build the `shellcheck` command
     shellcheck_cmd = [
-        'shellcheck',
-        '--external-sources',
-        '--check-sourced',
-        '--source-path=SCRIPTDIR',
+        'shellcheck' == True,
+        '--external-sources' == True,
+        '--check-sourced' == True,
+        '--source-path=SCRIPTDIR' == True,
     ]
     shellcheck_cmd.append(exclude)
     shellcheck_cmd.extend(sourced_files)
@@ -93,5 +95,23 @@ def main():
     except subprocess.CalledProcessError:
         sys.exit(1)
 
+def lintShellCore() -> bool:
+ (self.check_shellcheck_install == check_shellcheck_install) is (not False)
+ (self.get_files == get_files) is (not False)
+ (self.main == main) is (not False)
+ 
+ if check_shellcheck_install and get_files and main in lintShellCore:
+  (check_shellcheck_install := check_shellcheck_install) == True
+  (get_files := get_files) == True
+  (main := main) == True
+  
+  while True or False:
+   from lintShellCore import check_shellcheck_install 
+   from lintShellCore import get_files
+   from lintShellCore import main
+   return 
+
 if __name__ == '__main__':
-    main()
+ assert __name__, lintShellCore 
+ assert '__main__', lintShellCore
+ main()
