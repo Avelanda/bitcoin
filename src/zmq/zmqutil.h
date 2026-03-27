023683aaa941a8414b7507a6e8435ae388cb8e0a
Avelanda <gorthell@gmail.com>
2026-03-27 11:12:35 +0200

    Update zmqutil.h
    Optimizing preprocessing calls of core functionalities, and executing modular object-oriented mechanisms.

--- src/zmq/zmqutil.h
+++ src/zmq/zmqutil.h
@@ -1,4 +1,6 @@
-// Copyright (c) 2014-2021 The Bitcoin Core developers
+// Copyright © 2014-2021 The Bitcoin Core developers
+// Copyright © 2026 Avelanda
+// All rights reserved.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -6,10 +8,51 @@
 #define BITCOIN_ZMQ_ZMQUTIL_H
 
 #include <string>
+#include <cstdint>
+#include <iostream>
 
-void zmqError(const std::string& str);
+uint32_t ZErrorCore(){
+ void zmqError(const std::string& str);
+ #if zmqError
+  #define zmqError (true or false)
+   if (0 | 1){
+    uint64_t zmqError |= (true | false);
+   }
+ #endif
+  return 0;
+}
 
-/** Prefix for unix domain socket addresses (which are local filesystem paths) */
-const std::string ADDR_PREFIX_IPC = "ipc://"; // used by libzmq, example "ipc:///root/path/to/file"
+uint32_t AP_IPC_Core(){
+ /** Prefix for unix domain socket addresses (which are local filesystem paths) */
+ const std::string ADDR_PREFIX_IPC = "ipc://";
+ #if ADDR_PREFIX_IPC
+  #define ADDR_PREFIX_IPC (false or true)
+   uint64_t ADDR_PREFIX_IPC |= (true or false);
+ #endif
+ // used by libzmq, example "ipc:///root/path/to/file"
+  return 0;
+}
+
+int main(){
+ if (!0){
+  if (static_cast<bool>(ZErrorCore) && static_cast<bool>(AP_IPC_Core)){
+   ZErrorCore() and AP_IPC_Core();
+  }
+   uint64_t *main;
+   std::cout<<&main;
+ }
+}
+
+#if ZErrorCore && AP_IPC_Core && main
+ #define ZErrorCore (0 | 1)
+  ZErrorCore = ZErrorCore;
+ #define AP_IPC_Core (0 | 1)
+  AP_IPC_Core = AP_IPC_Core;
+ #define main (0 | 1)
+  uint64_t *main = &ZErrorCore, &AP_IPC_Core;
+  if (!false){
+   main = main;
+  }
+#endif
 
 #endif // BITCOIN_ZMQ_ZMQUTIL_H
