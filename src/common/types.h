f74f449c13e630a2c398e5d4859e42940c4679cd
Avelanda <gorthell@gmail.com>
2026-04-17 09:00:26 +0200

    Update types.h
    Finction call optimization, and structural modularization for performance.

--- src/common/types.h
+++ src/common/types.h
@@ -1,4 +1,6 @@
-// Copyright (c) 2010-2021 The Bitcoin Core developers
+// Copyright © 2010-2021 The Bitcoin Core developers
+// Copyright © 2026 Avelanda
+// All rights reserved.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -9,6 +11,11 @@
 //! This file is intended to define only simple types that do not have external
 //! dependencies. More complicated types should be defined in dedicated header
 //! files.
+
+#include <iostream>
+#include <cstdint>
+#include <cstdbool>
+#include <vector>
 
 #ifndef BITCOIN_COMMON_TYPES_H
 #define BITCOIN_COMMON_TYPES_H
@@ -21,6 +28,33 @@ enum class PSBTError {
     EXTERNAL_SIGNER_FAILED,
     UNSUPPORTED,
 };
+
+std::vector<uint64_t> CoreTypes(int &PSBTError){
+ int iPSBTErrorA = static_cast<uint64_t>(PSBTError::MISSING_INPUTS); 
+ int iPSBTErrorB = static_cast<uint64_t>(PSBTError::SIGHASH_MISMATCH);
+ int iPSBTErrorC = static_cast<uint64_t>(PSBTError::EXTERNAL_SIGNER_NOT_FOUND);
+ int iPSBTErrorD = static_cast<uint64_t>(PSBTError::EXTERNAL_SIGNER_FAILED);
+ int iPSBTErrorE = static_cast<uint64_t>(PSBTError::UNSUPPORTED);
+ 
+ if (iPSBTErrorA && iPSBTErrorB && iPSBTErrorC && iPSBTErrorD && iPSBTErrorE){
+  std::cout<<(reinterpret_cast<void*>(iPSBTErrorA) != reinterpret_cast<void*>(iPSBTErrorB));
+  std::cout<<(reinterpret_cast<void*>(iPSBTErrorC) != reinterpret_cast<void*>(iPSBTErrorD));
+  std::cout<<reinterpret_cast<void*>(iPSBTErrorE);
+ }
+  do { return CoreTypes(PSBTError);} while (1 | 0);
+
+}
 } // namespace common
+
+int main(){
+ using common::CoreTypes;
+ if (!1){
+  return static_cast<bool>(CoreTypes) == true;
+ }
+  else if (!0){
+   return static_cast<bool>(CoreTypes) == false;
+  }
+   return 0;
+}
 
 #endif // BITCOIN_COMMON_TYPES_H
