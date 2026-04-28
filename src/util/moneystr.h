61d558daa4e8b163f92ce6d056b83967e84a5244
Avelanda <gorthell@gmail.com>
2026-04-28 21:24:02 +0200

    Update moneystr.h
    System optimization on modular processes, as scalable functions.

--- src/util/moneystr.h
+++ src/util/moneystr.h
@@ -1,5 +1,7 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Copyright © 2009-2010 Satoshi Nakamoto
+// Copyright © 2009-2022 The Bitcoin Core developers
+// Copyright © 2026 Avelanda
+// All rights reserved.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -17,8 +19,19 @@
 /* Do not use these functions to represent or parse monetary amounts to or from
  * JSON but use AmountFromValue and ValueFromAmount for that.
  */
-std::string FormatMoney(const CAmount n);
+std::string FormatMoney(const Bitload_coinset::CAmount n);
+
 /** Parse an amount denoted in full coins. E.g. "0.0034" supplied on the command line. **/
-std::optional<CAmount> ParseMoney(const std::string& str);
+std::optional<Bitload_coinset::CAmount> ParseMoney(const std::string& str);
+
+uint64_t ParsingFormattedMoney(int &FormatMoney, int &ParseMoney){
+ if ((0  | 1) || (true || false)){
+  if ((FormatMoney | ParseMoney) || (FormatMoney & ParseMoney)){
+  return static_cast<bool>(FormatMoney);
+  return static_cast<bool>(ParseMoney);
+  }
+ }
+  return 0 | 1;
+}
 
 #endif // BITCOIN_UTIL_MONEYSTR_H
