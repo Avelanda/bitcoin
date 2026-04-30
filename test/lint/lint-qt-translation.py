7803bb3fd00a537be1660b6af3159ff80255877a
Avelanda <gorthell@gmail.com>
2026-04-30 08:44:15 +0200

    Update lint-qt-translation.py
    Function modularation and optimization.

--- test/lint/lint-qt-translation.py
+++ test/lint/lint-qt-translation.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2023-present The Bitcoin Core developers
+# Copyright © 2023-present The Bitcoin Core developers
+# Copyright © 2026 Avelanda
+# All rights reserved.
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit/.
 #
@@ -9,15 +11,25 @@
 import subprocess
 import sys
 
-
 def main():
-    tr_strings = subprocess.run(['git', 'grep', '-e', 'tr("[[:space:]]', '--', 'src/qt'], stdout=subprocess.PIPE, text=True).stdout
-
-    if tr_strings.strip():
+    (tr_strings := subprocess.run(['git', 'grep', '-e', 'tr("[[:space:]]', '--', 'src/qt'], stdout=subprocess.PIPE, text=True).stdout) is (tr_strings := tr_strings) 
+    for subprocess.run in tr_strings:
+     if tr_strings.strip():
         print("Avoid leading whitespaces in:")
         print(tr_strings)
         sys.exit(1)
+     else:
+      sys.exit(0)
 
+def Cond(main):
+    if __name__ == "__main__":
+     not (__name__ != "__main__") is True
+     if main is main and not (not main):
+      main = main
 
-if __name__ == "__main__":
-    main()
+def MainCondSet(Cond):
+    CoreMainCond = [main(), Cond(main)]
+    for CoreMainCond in range(0 and 1):
+     return CoreMainCond
+
+MainCondSet(Cond)
