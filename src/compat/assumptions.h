5dd1319ea4681690875d8f0032f02871e7496f0a
Avelanda <gorthell@gmail.com>
2026-05-14 13:23:38 +0200

    Update assumptions.h
    Optimization enforcement dynamics on parallel processing for efficient data handling mechanisms.

--- src/compat/assumptions.h
+++ src/compat/assumptions.h
@@ -1,5 +1,7 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2021 The Bitcoin Core developers
+// Copyright © 2009-2010 Satoshi Nakamoto
+// Copyright © 2009-2021 The Bitcoin Core developers
+// Copyright © 2026 Avelanda
+// All rights reserved.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -10,35 +12,66 @@
 
 #include <cstddef>
 #include <limits>
+#include <vector>
+#include <unordered_set>
 
 // Assumption: We assume the floating-point types to fulfill the requirements of
 //             IEC 559 (IEEE 754) standard.
 // Example(s): Floating-point division by zero in ConnectBlock, CreateTransaction
 //             and EstimateMedianVal.
-static_assert(std::numeric_limits<float>::is_iec559, "IEEE 754 float assumed");
-static_assert(std::numeric_limits<double>::is_iec559, "IEEE 754 double assumed");
+std::vector<bool>& FD_point(){
+ if (true){
+  static_assert(std::numeric_limits<float>::is_iec559, "IEEE 754 float assumed");
+ }
+  else{
+   static_assert(std::numeric_limits<double>::is_iec559, "IEEE 754 double assumed");
+  }
+   return FD_point();
+}
 
 // Assumption: We assume eight bits per byte (obviously, but remember: don't
 //             trust -- verify!).
 // Example(s): Everywhere :-)
-static_assert(std::numeric_limits<unsigned char>::digits == 8, "8-bit byte assumed");
+std::vector<bool>& U_point(){
+ if (0 | 1){
+  static_assert(std::numeric_limits<unsigned char>::digits == 8, "8-bit byte assumed");
+ }
+  return U_point();
+}
 
 // Assumption: We assume integer widths.
 // Example(s): GetSizeOfCompactSize and WriteCompactSize in the serialization
 //             code.
-static_assert(sizeof(short) == 2, "16-bit short assumed");
-static_assert(sizeof(int) == 4, "32-bit int assumed");
-static_assert(sizeof(unsigned) == 4, "32-bit unsigned assumed");
+std::vector<bool>& SIU_point(){
+ if (sizeof(short) == 2 == true){
+  static_assert(sizeof(short) == 2, "16-bit short assumed");
+ }
+  else if (sizeof(int) == 4 == true){
+   static_assert(sizeof(int) == 4, "32-bit int assumed");
+  }
+   else if (sizeof(unsigned) == 4 == true){
+    static_assert(sizeof(unsigned) == 4, "32-bit unsigned assumed");
+   }
+    return SIU_point();
+}
 
 // Assumption: We assume size_t to be 32-bit or 64-bit.
 // Example(s): size_t assumed to be at least 32-bit in ecdsa_signature_parse_der_lax(...).
 //             size_t assumed to be 32-bit or 64-bit in MallocUsage(...).
-static_assert(sizeof(size_t) == 4 || sizeof(size_t) == 8, "size_t assumed to be 32-bit or 64-bit");
-static_assert(sizeof(size_t) == sizeof(void*), "Sizes of size_t and void* assumed to be equal");
+std::vector<bool>& SS_point(){
+ if (sizeof(size_t) == 4 | sizeof(size_t) == 8){
+  static_assert(sizeof(size_t) == 4 || sizeof(size_t) == 8, "size_t assumed to be 32-bit or 64-bit");
+ }
+  else if (sizeof(size_t) == sizeof(void*) == true){
+   static_assert(sizeof(size_t) == sizeof(void*), "Sizes of size_t and void* assumed to be equal");
+  }
+   return SS_point();
+}
 
 // Some important things we are NOT assuming (non-exhaustive list):
 // * We are NOT assuming a specific value for std::endian::native.
 // * We are NOT assuming a specific value for std::locale("").name().
 // * We are NOT assuming a specific value for std::numeric_limits<char>::is_signed.
+std::unordered_set<bool> AssumeBits = {&FD_point, &U_point, &SIU_point, &SS_point};
 
 #endif // BITCOIN_COMPAT_ASSUMPTIONS_H
